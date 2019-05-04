89e0812ee486cc90f5e3c920ff9434811ff92319
Armando Salcido <aksalcid@uci.edu>
2019-05-04 15:06:25 -0700

    Completed

--- Twitter/Controllers/TweetViewController.swift
+++ Twitter/Controllers/TweetViewController.swift
@@ -8,15 +8,36 @@
 
 import UIKit
 
-class TweetViewController: UIViewController {
+class TweetViewController: UIViewController, UITextViewDelegate {
 
     @IBOutlet weak var tweetTextView: UITextView!
+    let tweetIntroduction: String = "What's happening?"
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tweetTextView.becomeFirstResponder()
+        
+        self.tweetTextView.delegate = self
+        self.tweetTextView.text = self.tweetIntroduction
+        self.tweetTextView.textColor = UIColor.lightGray
+        
+        self.tweetTextView.selectedTextRange = self.tweetTextView.textRange(from: self.tweetTextView.beginningOfDocument, to: self.tweetTextView.beginningOfDocument)
     }
   
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = nil
+            textView.textColor = UIColor.black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = self.tweetIntroduction
+            textView.textColor = UIColor.lightGray
+        }
+    }
+    
     @IBAction func back(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
@@ -24,7 +45,6 @@ class TweetViewController: UIViewController {
     @IBAction func tweet(_ sender: Any) {
         // Only run the function is text is not empty
         if (!tweetTextView.text.isEmpty) {
-            
             TwitterAPICaller.client?.postTweet(tweetString: tweetTextView.text, success: {
                 self.dismiss(animated: true, completion: nil)}, failure: { (Error) in
                 print("Error Posting Tweet! \(Error)")
